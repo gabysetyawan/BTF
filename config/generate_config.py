5ac2e264a9411c0a25b5ba8238bda7def7a614f5
gabysetyawan <gabrielanss19@gmail.com>
2021-10-16 10:51:27 -0500

    Update generate_config.py
    Created skeleton for config objects for device infos

--- config/generate_config.py
+++ config/generate_config.py
@@ -6,3 +6,19 @@ config_object = ConfigParser()
 
 # TODO   We need a DEVICE_LIST to iterate on (has Devices)
 #        Devices will have contents modeled in Device.py
+
+# devices we have: BBB and raspberry pi
+
+#Assume we ne need a sections in the config file for the devices, call them DEVICEINFO
+config_object["DEVICEINFO"]={
+    #fill in required device info here
+}
+
+#Write the above section to config.ini file
+with open('config.ini', 'w') as conf:
+    config_object.write(conf)
+
+
+*generate_config.py 
+
+
